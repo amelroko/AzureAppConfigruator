7a11e1dc10c83c4e2421941e8ab50091bea7041d
amel-burch <amel.karsic@stu.ibu.edu.ba>
2024-06-11 10:48:49 +0200

    Add project files.

---
+++ TestAzureAppConfigruator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace TestAzureAppConfigruator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = System.Configuration.ConfigurationManager.AppSettings["dev-key"];
+            string message2 = System.Configuration.ConfigurationManager.AppSettings["base-key"];
+            Console.WriteLine(message);
+            Console.WriteLine(message2);
+
+            Thread.Sleep(3000);
+
+            //string message3 = System.Configuration.ConfigurationManager.AppSettings["base-amel"];
+
+            //Console.WriteLine(message3);
+
+            //Thread.Sleep(1000);
+        }
+    }
+}
